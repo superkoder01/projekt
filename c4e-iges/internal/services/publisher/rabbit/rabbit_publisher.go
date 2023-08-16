d41357e3aca3bd32103360269450153617b8b838
superkoder01 <xyzabc_kodowanie@o2.pl>
2023-08-16 12:48:01 +0200

    License added to BM.

--- c4e-iges/internal/services/publisher/rabbit/rabbit_publisher.go
+++ c4e-iges/internal/services/publisher/rabbit/rabbit_publisher.go
@@ -1,3 +1,21 @@
+/*
+BMSFES. 
+Copyright (C) 2022-2035 
+
+This file is part of the BMSFES solution. 
+BMSFES is free software: you can redistribute it and/or modify 
+it under the terms of the GNU Affero General Public License as 
+published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version.
+ 
+BMSFES solution is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License 
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package rabbit_publisher
 
 import (
